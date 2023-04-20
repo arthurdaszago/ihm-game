65c47470104338a0e9312b3bc6d17f8b39a16191
Arthur Zago <arthurzago284@gmail.com>
2023-04-19 23:33:49 -0300

    added entire project

---
+++ Assets/Scripts/Bird/Bird.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private GameObject plane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.FindGameObjectWithTag("Plane");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Border")
+        {
+            Destroy(this.gameObject);
+        } 
+        
+        else if(collision.tag == "Plane")
+        {   
+            Destroy(plane.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Plane/Plane.cs
@@ -0,0 +1,85 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    public float planeSpeed;
+
+    private Rigidbody2D rb;
+    private Vector2 planeDirection;
+
+    public float rateRotation = 0.15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionY = Input.GetAxisRaw("Vertical");
+        planeDirection = new Vector2(0, directionY).normalized;
+
+        float planeY = rb.position.y;
+
+        UnityEngine.Debug.Log(planeY);
+
+        if (directionY == 1)
+        {
+            if (rb.rotation < 25 && planeY <= 2.75f)
+            {
+                rb.rotation += rateRotation;
+            }
+            else
+            {
+                if (rb.rotation > 0)
+                {
+                    rb.rotation -= rateRotation;
+                }
+                else 
+                {
+                    rb.rotation += rateRotation;
+                }
+            }
+        }
+        else if (directionY == -1)
+        {
+            if (rb.rotation > -25 && planeY >= -2.75f)
+            {
+                rb.rotation -= rateRotation;
+            }
+            else
+            {
+                if (rb.rotation > 0)
+                {
+                    rb.rotation -= rateRotation;
+                }
+                else 
+                {
+                    rb.rotation += rateRotation;
+                }
+            }
+        }
+
+        else 
+        {
+            if (rb.rotation > 0)
+            {
+                rb.rotation -= rateRotation;
+            }
+            else 
+            {
+                rb.rotation += rateRotation;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, planeDirection.y * planeSpeed);
+    }
+}
